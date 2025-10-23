e474311c1055d8b2fe1f803cbafeeccd2a433d75
loucass003 <loucass003@gmail.com>
2025-10-23 05:03:47 +0200

    Fix ulauncher

--- home-modules/ulauncher/default.nix
+++ home-modules/ulauncher/default.nix
@@ -10,23 +10,34 @@
     ulauncher
   ];
 
-  systemd.user.services.ulauncher = {
-    Unit = {
-      Description = "Linux Application Launcher";
-      Documentation = [ "https://ulauncher.io/" ];
+  # Configure GNOME keyboard shortcut for ulauncher
+  dconf.settings = {
+    "org/gnome/settings-daemon/plugins/media-keys" = {
+      custom-keybindings = [
+        "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/"
+      ];
     };
-
-    Service = {
-      Type = "Simple";
-      Restart = "Always";
-      RestartSec = 1;
-      ExecStart = "${pkgs.ulauncher}/bin/ulauncher --hide-window";
-    };
-
-    Install = {
-      WantedBy = [ "graphical-session.target" ];
+    
+    "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0" = {
+      binding = "<Primary>space";
+      command = "${pkgs.ulauncher}/bin/ulauncher-toggle";
+      name = "Ulauncher";
     };
   };
+
+  xdg.configFile."autostart/ulauncher.desktop".text = ''
+    [Desktop Entry]
+    Name=Ulauncher
+    Comment=Application launcher for Linux
+    GenericName=Launcher
+    Categories=GNOME;GTK;Utility;
+    TryExec=${pkgs.ulauncher}/bin/ulauncher
+    Exec=${pkgs.ulauncher}/bin/ulauncher
+    Icon=ulauncher
+    Terminal=false
+    Type=Application
+    X-GNOME-Autostart-enabled=true
+  '';
 
   home.file."${config.home.homeDirectory}/.config/ulauncher/settings.json" = {
     source = ./settings.json;
