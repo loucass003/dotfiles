bf73140b31e52887c54712c7a5f54d341abae54b
loucass003 <loucass003@gmail.com>
2025-10-26 07:32:49 +0100

    XDRP remove

--- hardware/config-commons.nix
+++ hardware/config-commons.nix
@@ -141,21 +141,6 @@
 
   services.teamviewer.enable = true;
 
-  services.xrdp.enable = true;
-
-  # Use the GNOME Wayland session
-  services.xrdp.defaultWindowManager = "${pkgs.gnome-session}/bin/gnome-session";
-
-  # XRDP needs the GNOME remote desktop backend to function
-  services.gnome.gnome-remote-desktop.enable = true;
-
-  # Open the default RDP port (3389)
-  services.xrdp.openFirewall = true;
-
-  # Disable autologin to avoid session conflicts
-  services.displayManager.autoLogin.enable = false;
-  services.getty.autologinUser = null;
-
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
